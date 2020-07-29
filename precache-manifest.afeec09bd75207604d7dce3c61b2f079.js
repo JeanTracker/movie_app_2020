60ffe77377a6f8fc5049a3b99ae5e8972658bb74
JeanTracker <hyojoong.kim.jean@gmail.com>
2020-07-29 14:55:17 +0900

    Updates

---
+++ precache-manifest.afeec09bd75207604d7dce3c61b2f079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82627b33c67ef7ea05b2875428626a20",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7c8c7ffbcc9dc287312f",
+    "url": "/movie_app_2020/static/css/main.c7a22424.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8c7ffbcc9dc287312f",
+    "url": "/movie_app_2020/static/js/main.57d84c45.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
